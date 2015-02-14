9d1b6634b8dd6e49b4a156d8c5360d2436447548
Milano Gadelha Carvalho <milanogc@gmail.com>
2015-02-14 11:31:20 -0300

    Simple refactoring.

--- src/test/java/milanogc/accounting/domain/account/PostingTest.java
+++ src/test/java/milanogc/accounting/domain/account/PostingTest.java
@@ -1,5 +1,6 @@
 package milanogc.accounting.domain.account;
 
+import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableSet;
 
 import org.junit.Test;
@@ -11,22 +12,26 @@ public class PostingTest {
   private Account account = new Account.Builder(new AccountId(""), "ASSET", new Date()).build();
 
   @Test(expected = Posting.EmptyEntries.class)
-  public void createPosting() {
-    new Posting(new PostingId(""), new Date(), ImmutableSet.of());
+  public void postingWithoutEntries_ShouldThrowEmptyEntries() {
+    createPosting(ImmutableSet.of());
   }
 
   @Test(expected = Posting.NotBalancedEntries.class)
   public void postingOfOneEntry_ShouldThrowNotBalancedEntries() {
-    new Posting(new PostingId(""), new Date(), ImmutableSet.of(new Entry(account, 1)));
+    createPosting(ImmutableSet.of(new Entry(account, 1)));
   }
 
   @Test(expected = Posting.NotBalancedEntries.class)
   public void postingOfTwoUnbalancedEntries_ShouldThrowNotBalancedEntries() {
-    new Posting(new PostingId(""), new Date(), ImmutableSet.of(new Entry(account, 1), new Entry(account, 1)));
+    createPosting(ImmutableSet.of(new Entry(account, 1), new Entry(account, 1)));
   }
 
   @Test
   public void postingOfTwoBalancedEntries() {
-    new Posting(new PostingId(""), new Date(), ImmutableSet.of(new Entry(account, 1), new Entry(account, -1)));
+    createPosting(ImmutableSet.of(new Entry(account, 1), new Entry(account, -1)));
+  }
+
+  private void createPosting(ImmutableCollection<Entry> entries) {
+    new Posting(new PostingId(""), new Date(), entries);
   }
 }
